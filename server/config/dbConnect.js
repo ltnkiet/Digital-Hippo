dcb3fc5aa0240a3668e7c4c5ec4397a91285428b
ltnkiet <89096536+ltnkiet@users.noreply.github.com>
2024-01-08 00:48:52 +0700

    update(*)

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -2,10 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI_PRODUCTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI_PRODUCTION);
     if (conn.connection.readyState == 1)
       console.log("DB connection is successfully");
     else console.log("DB connection failed");
