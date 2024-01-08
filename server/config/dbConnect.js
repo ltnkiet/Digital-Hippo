de4f1ad7fc814cc3e6b61331a969e002fc2e8d0b
ltnkiet <89096536+ltnkiet@users.noreply.github.com>
2024-01-08 17:14:40 +0700

    config db

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI_PRODUCTION);
+    const conn = await mongoose.connect(process.env.MONGODB_URI_DEVELOPMENT);
     if (conn.connection.readyState == 1)
       console.log("DB connection is successfully");
     else console.log("DB connection failed");
